02a14568598cfbddbe33cdf83bde764949e28922
Elisabeth Hendrickson <esh@qualitytree.com>
2020-03-26 21:40:09 -0700

    More granular handling of lookahead cases

--- solve_by_walk_paths.py
+++ solve_by_walk_paths.py
@@ -22,28 +22,40 @@ class Solver:
                 # we're done, got it in 1
                 self.paths.append([move])
             else:
-                self.paths.append(self.walk_paths_from_state(resulting_board, [move], [self.board.tiles,resulting_board]))
+                possible_path = self.walk_paths_from_state(resulting_board, [move], [self.board.tiles,resulting_board])
+                if (possible_path != None):
+                    self.paths.append(possible_path)
         return self.paths
 
     def walk_paths_from_state(self, board_state, moves, visitedstates):
+        print("\nWALKING PATHS\nVISITED STATES:\n{}\nMOVES: {}\n".format(visitedstates, moves))
         newwhatifs = Board(board_state).lookahead()
         for newmove in newwhatifs:
+            print("considering move: {}".format(newmove))
             newboard = newwhatifs[newmove]
             if (newboard == self.board.solution):
                 # we're done
                 moves.append(newmove)
+                print("Done - found the exit")
                 return moves
+            elif (newmove == moves[-1]):
+                # ignore this move - it just takes us back
+                print("Ignoring reversal")
+                pass
             elif (not newboard in visitedstates):
                 # let's go again
-                visitedstates.append(newboard)
+                print("Not done, and not going somewhere we've been before")
                 moves.append(newmove)
+                visitedstates.append(newboard)
                 return self.walk_paths_from_state(newwhatifs[newmove], moves, visitedstates)
             else:
                 # this approach won't work
-                pass
+                print("Not a viable path")
+                return None
 
     def find_shortest_path(self):
         all_paths = self.find_paths()
+        print("FINDING SHORTEST\npaths:\n{}".format(all_paths))
         shortest_path = None
         for path in all_paths:
             if (shortest_path == None) or (len(shortest_path) > len(path)):
--- test_solve_by_walk_paths.py
+++ test_solve_by_walk_paths.py
@@ -6,15 +6,15 @@ def test_solver_has_board():
     player = Solver([1,2,3,None])
     assert isinstance(player.board, Board)
 
-def test_solver_chooses_the_move_that_solves_game():
-    player = Solver([1, 2, None, 3])
-    player.solve()
-    assert player.board.solved()
-
-def test_solver_can_solve_a_game_requiring_rotation():
-    player = Solver([1, None, 3, 2])
-    player.solve()
-    assert player.board.solved()
+# def test_solver_chooses_the_move_that_solves_game():
+#     player = Solver([1, 2, None, 3])
+#     player.solve()
+#     assert player.board.solved()
+#
+# def test_solver_can_solve_a_game_requiring_rotation():
+#     player = Solver([1, None, 3, 2])
+#     player.solve()
+#     assert player.board.solved()
 
 def test_find_paths_can_map_solutions():
     player = Solver([1, 2, None, 3])
@@ -32,8 +32,12 @@ def test_solver_can_solve_all_valid_2x2s():
         player.solve()
         assert player.board.solved()
 
+def test_solver_can_solve_a_3x3_requiring_1_move():
+    player = Solver([1, 2, 3, 4, 5, 6, 7, None, 8])
+    player.solve()
+    assert player.board.solved()
+
 # def test_solver_can_solve_a_3x3_requiring_5_moves():
-#     # Note that this is theoretically no more difficult than the 2x2
 #     player = Solver([1, 2, 3, 4, 6, 8, 7, None, 5])
 #     player.solve()
 #     assert player.board.solved()
