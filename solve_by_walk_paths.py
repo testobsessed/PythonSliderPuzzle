b2e3be83676279803ef0db3354a86cb52291d7fb
Elisabeth Hendrickson <esh@qualitytree.com>
2020-03-26 18:02:19 -0700

    New solution approach - brute force

---
+++ solve_by_walk_paths.py
@@ -0,0 +1,45 @@
+from slider_game import Board
+import random
+
+
+class Solver:
+    def __init__(self, tile_config):
+        self.board = Board(tile_config)
+        self.solutions = {}
+        self.recursed = 0
+        self.paths = []
+
+    def solve(self):
+        moves = self.find_shortest_path()
+        for move in moves:
+            self.board.move(move)
+
+    def find_paths(self):
+        whatifs = self.board.lookahead()
+        for move in whatifs:
+            resulting_board = whatifs[move]
+            if (resulting_board == self.board.solution):
+                # we're done, got it in 1
+                self.paths.append([move])
+            else:
+                self.paths.append(self.walk_paths_from_state(resulting_board, [move]))
+        return self.paths
+
+    def walk_paths_from_state(self, board_state, moves):
+        newwhatifs = Board(board_state).lookahead()
+        for newmove in newwhatifs:
+            newboard = newwhatifs[newmove]
+            if (newboard == self.board.solution):
+                moves.append(newmove)
+                return moves
+            elif (newmove != moves[-1]):
+                moves.append(newmove)
+                return self.walk_paths_from_state(newwhatifs[newmove], moves)
+
+    def find_shortest_path(self):
+        all_paths = self.find_paths()
+        shortest_path = None
+        for path in all_paths:
+            if (shortest_path == None) or (len(shortest_path) > len(path)):
+                shortest_path = path
+        return shortest_path
---
+++ test_solve_by_walk_paths.py
@@ -0,0 +1,39 @@
+from solve_by_walk_paths import Solver
+from slider_game import Board
+import pytest
+
+def test_solver_has_board():
+    player = Solver([1,2,3,None])
+    assert isinstance(player.board, Board)
+
+def test_solver_chooses_the_move_that_solves_game():
+    player = Solver([1, 2, None, 3])
+    player.solve()
+    assert player.board.solved()
+
+def test_solver_can_solve_a_game_requiring_rotation():
+    player = Solver([1, None, 3, 2])
+    player.solve()
+    assert player.board.solved()
+
+def test_find_paths_can_map_solutions():
+    player = Solver([1, 2, None, 3])
+    paths = player.find_paths()
+    assert paths == [[1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2], [3]]
+
+def test_find_shortest_path():
+    player = Solver([1, 2, None, 3])
+    assert player.find_shortest_path() == [3]
+
+def test_solver_can_solve_all_valid_2x2s():
+    boards = Board.get_boards(2)
+    for board in boards:
+        player = Solver(board)
+        player.solve()
+        assert player.board.solved()
+
+# def test_solver_can_solve_a_3x3_requiring_5_moves():
+#     # Note that this is theoretically no more difficult than the 2x2
+#     player = Solver([1, 2, 3, 4, 6, 8, 7, None, 5])
+#     player.solve()
+#     assert player.board.solved()
